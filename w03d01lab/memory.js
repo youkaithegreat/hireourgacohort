2747b48eca2b8248370a5b9322dfc0fe07126fde
Kevin Yang <kevinyang@Kevins-Air.attlocal.net>
2022-07-25 13:33:28 -0500

    Stuff

--- w03d01lab/memory.js
+++ w03d01lab/memory.js
@@ -10,7 +10,10 @@ const player = {
     currentBet: 0
 }
 
-const dealerHand = [{}]
+const dealer =  {
+    hand: [{}],
+    hold: false
+}
 
 const createDeck = () => {
     for(let i = 0; i < suite.length;i++){
@@ -60,11 +63,16 @@ const shuffle = () => {
         }
 
         const resetGame = () => {
+
+
+            $("h1").text("Lost")
                 $(".playerHandCards").attr("src","primary/2B.svg")
                 $(".dealerHandCards").attr("src","primary/2B.svg")
   
-            player.hand =[{}]
-            dealerHand = [{}]
+            for(let i = 0; i< player.hand.length; i++){
+                player.hand.pop()
+                dealer.hand.pop()
+            }
             $(".playerHandCards").remove()  
             $(".dealerHandCards").remove()
             playBlackJack()
@@ -73,7 +81,7 @@ const shuffle = () => {
     
         const checkPlayerBust = () => {
             let playerSum = 0;
-            for(let i = 1; i < player.hand.length; i++){
+            for(let i = 0; i < player.hand.length; i++){
                 playerSum += player.hand[i].value
             }
             if(playerSum > 21){
@@ -84,17 +92,18 @@ const shuffle = () => {
     
         const dealerHandCheck = () => {
             let dealerSum = 0;
-            for(let i = 1; i < dealerHand.length; i++){
-                dealerSum+=dealerHand[i].value
+            for(let i = 0; i < dealer.hand.length; i++){
+                dealerSum+=dealer.hand[i].value
             }
             if(dealerSum >=17){
                 //nothing happens
                 console.log("Dealer has reached 17")
             }else{
                 console.log("dealer receives another card")
-                dealerHand.push(deck.pop())
-                $('#dealerHandContainer').append(`<img src ='${dealerHand[dealerHand.length-1].imgSrc}' class='dealerHandCards'>`)
+                dealer.hand.push(deck.pop())
+                $('#dealerHandContainer').append(`<img src ='${dealer.hand[dealer.hand.length-1].imgSrc}' class='dealerHandCards'>`)
             }
+            dealerSum = 0
         }
     
         const stand =()=> {
@@ -105,8 +114,9 @@ const shuffle = () => {
     
         const hit = () => {
             player.hand.push(deck.pop())
-            const $newCard = $("<img>").attr({"src":`${player.hand[player.hand.length-1].imgSrc}`,"class":"playerHandCards"})
+            const $newCard = $("<img>").attr("src",`${player.hand[player.hand.length-1].imgSrc}`)
             $("#playerHandContainer").append($newCard)
+            alert("Should get a card before dealer does")
             checkPlayerBust()
             dealerHandCheck()
             makeSelection()
@@ -148,25 +158,28 @@ const shuffle = () => {
     
         const initialDeal = () => {
             
-            player.hand.push(deck.pop())
-            const $playerCardOne = $('<img>').attr("src",`${player.hand[1].imgSrc}`).addClass("playerHandCards")
+            player.hand[0] = deck.pop()
+            const $playerCardOne = $('<img>').attr("src",`${player.hand[0].imgSrc}`).addClass("playerHandCards")
             $('#playerHandContainer').append($playerCardOne)
 
-            dealerHand.push(deck.pop())
+            dealer.hand[0] = deck.pop()
             const $dealerCardOne = $('<img>').attr("src","primary/2B.svg").addClass("dealerHandCards")
             $('#dealerHandContainer').append($dealerCardOne)
 
-            player.hand.push(deck.pop())
-            const $playerCardTwo = $('<img>').attr("src",`${player.hand[2].imgSrc}`).addClass("playerHandCards")
+            player.hand[1] = deck.pop()
+            const $playerCardTwo = $('<img>').attr("src",`${player.hand[1].imgSrc}`).addClass("playerHandCards")
             $('#playerHandContainer').append($playerCardTwo)
 
-            dealerHand.push(deck.pop())
-            const $dealerCardTwo = $('<img>').attr("src",`${dealerHand[2].imgSrc}`).addClass("dealerHandCards")
+            dealer.hand[1] = deck.pop()
+            const $dealerCardTwo = $('<img>').attr("src",`${dealer.hand[1].imgSrc}`).addClass("dealerHandCards")
             $('#dealerHandContainer').append($dealerCardTwo)
+            console.log(dealer.hand)
+            console.log(player + "Player")
 
         }
     
         const playBlackJack = () => {
+            createDeck()
             shuffle()
              //betting
             initialDeal()
